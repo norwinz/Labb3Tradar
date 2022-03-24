3a69afdd6a4cf07b7da07ddce61ec5f94b5ff86c
norwinz <dan-gustafsson@hotmail.com>
2022-03-24 13:36:10 +0100

    Add project files.

---
+++ Labb3Tradar/Car.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Labb3Tradar
+{
+   public class Car
+    {
+        public string Name { get; set; }
+        public double Speed { get; set; }
+        public double Distance { get; set; }
+        public double raceLength { get; set; }
+        public bool winner { get; set; }
+        public int carStop { get; set; }
+        public Car(string _Name)
+        {
+            Name = _Name;
+            Speed = 120; //in km/h
+            Distance = 0; //in meters
+            raceLength = 10000; //in meters
+            winner = false;
+            carStop = 1; //Value will be set to 0 if the car is forced to stop
+        }
+
+        public void race()
+        { 
+            Thread problemThread = new Thread(new ThreadStart(problem));
+            problemThread.Start();
+            while(Distance < raceLength)
+            {
+                Thread.Sleep(1000);
+                Distance += (Speed*0.27777778) * carStop;  //Km/h --> m/s conversion. Carstop will be set to 0 if car stops.
+            }
+            winner = true;        
+            Console.WriteLine("{0} finish!", Name);
+        }
+        public void problem()
+        {                  
+                while (true)
+                {
+                    Thread.Sleep(30000);
+                    Random rnd = new Random();
+                    int problemRnd = rnd.Next(1, 51);
+                    if (Distance > raceLength)
+                    {
+                    //Stop Problems to show if race is finished
+                    }
+                    else
+                    {                    
+                    if (problemRnd == 1)
+                    {
+                        Thread refuelThread = new Thread(new ThreadStart(problemRefuel));
+                        refuelThread.Start();
+                    }
+                    if (problemRnd == 2 || problemRnd == 3)
+                    {
+                        Thread flatThread = new Thread(new ThreadStart(problemFlat));
+                        flatThread.Start();
+                    }
+                    if (problemRnd >= 4 && problemRnd <= 8)
+                    {
+                        Thread birdThread = new Thread(new ThreadStart(problemBird));
+                        birdThread.Start();
+                    }
+                    if (problemRnd >= 9 && problemRnd <= 18) 
+                    {
+                        problemEngine();
+                    }
+                    if (problemRnd >= 19)
+                    {
+                        Console.WriteLine("{0} keeps racing. Go Go Go!", Name);
+                    }
+                }
+            }
+            
+        }
+        public void problemRefuel()
+        {
+            Console.WriteLine("{0} needs to refuel, it will take 30 seconds!", Name);
+            carStop = 0;
+            Thread.Sleep(30000);
+            Console.WriteLine("Refueling done! {0} continues the race", Name);
+            carStop = 1;
+
+        }
+        public void problemFlat()
+        {
+            Console.WriteLine("Oh no! {0} has a flat tire, it will take 20 seconds to change to a new one!", Name);
+            carStop = 0;
+            Thread.Sleep(20000);
+            Console.WriteLine("Tire change is done! {0} continues the race", Name);
+            carStop = 1;
+        }
+        public void problemBird()
+        {
+            Console.WriteLine("BIRD STRIKE! A bird have hit the windshield. {0} need to stop and clean it, it will take 10 seconds!", Name);
+            carStop = 0;
+            Thread.Sleep(10000);
+            Console.WriteLine("Windshield is cleaned! {0} continues the race", Name);
+            carStop = 1;
+        }
+        public void problemEngine()
+        {
+            Speed -= 1;
+            Console.WriteLine("The engine have lost power! {0} will drive slower for the rest of the race. Current speed is: {1} km/h", Name, Speed);           
+            
+        }
+        public void changeName()
+        {
+            Console.WriteLine("New name: ");
+            Name = Console.ReadLine();
+        }
+    }
+}
---
+++ Labb3Tradar/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Labb3Tradar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car CarA = new Car("Anas");
+            Car CarB = new Car("Tobias");
+            List<Car> AllCars = new List<Car>();
+            AllCars.Add(CarA);
+            AllCars.Add(CarB);
+            
+            
+                
+                Thread CarAThread = new Thread(new ThreadStart(CarA.race));
+                Thread CarBThread = new Thread(new ThreadStart(CarB.race));
+                Thread WinnerThread = new Thread(new ThreadStart(delegate()
+                {
+                bool WeHaveAWinner = false;
+                while (!WeHaveAWinner)
+                {
+                    foreach (Car item in AllCars)
+                    {
+                        if (item.winner == true)
+                        {
+                            Console.WriteLine("{0} won the race!", item.Name);
+                            WeHaveAWinner = true;
+                        }
+                    }
+                }
+            }));
+            while (true)
+            { 
+                Console.WriteLine(
+                "1.Start the race!\n" +
+                "2. Change Racer 1 name ({0})\n" +
+                "3. Change Racer 2 name ({1})\n" +
+                "4. Change Car Speed ({2} km/h)\n" +
+                "5. Change Race Distance ({3} km)", CarA.Name, CarB.Name, CarA.Speed, CarA.raceLength/1000);
+
+                string menuOption = Console.ReadLine();
+                 switch (menuOption)
+                {
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Race starts in 3:");
+                            Thread.Sleep(1000);
+                            Console.WriteLine("2");
+                            Thread.Sleep(1000);
+                            Console.WriteLine("1");
+                            Thread.Sleep(1000);
+                            Console.WriteLine("GO!");                            
+                            CarAThread.Start();
+                            CarBThread.Start();
+                            WinnerThread.Start();
+                            Console.WriteLine("Press enter to show race details");
+                            foreach (Car item in AllCars)
+                            {
+                                while (item.Distance <= item.raceLength)
+                                {
+                                    Console.ReadKey();
+                                    raceStatus(AllCars);
+                                    Console.WriteLine();
+                                }
+                            }
+                            Console.Clear();
+                            continue;
+                        }
+                    case "2":
+                        {
+                            CarA.changeName();
+                            Console.Clear();
+                            continue;
+                        }
+                    case "3":
+                        {
+                            CarB.changeName();
+                            Console.Clear();
+                            continue;
+                        }
+                    case "4":
+                        {
+                            bool correctspeed = false;
+                            double newspeed=120;
+                            while (!correctspeed)
+                            {
+                                Console.WriteLine("Change speed of the cars: (km/h)");
+                                try
+                                {
+                                    newspeed = Convert.ToDouble(Console.ReadLine());
+                                    correctspeed = true;
+                                }
+                                catch
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Need to be a number");
+                                    correctspeed = false;
+                                }
+                                
+                            }
+                            CarA.Speed = newspeed;
+                            CarB.Speed = newspeed;
+                            Console.Clear();
+                            continue;
+                        }
+                    case "5":
+                        {
+                            bool correctRaceLength = false;
+                            double newRaceLength = 10000;
+                            while (!correctRaceLength)
+                            {
+                                Console.WriteLine("Change the distance of the race: (km)");
+                                try
+                                {
+                                    newRaceLength = Convert.ToDouble(Console.ReadLine());
+                                    correctRaceLength = true;
+                                }
+                                catch
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("Need to be a number");
+                                    correctRaceLength = false;
+                                }
+
+                            }
+                            newRaceLength = newRaceLength * 1000;
+                            CarA.raceLength = newRaceLength;
+                            CarB.raceLength = newRaceLength;
+                            Console.Clear();
+                            continue;
+                        }
+                    default:
+                        {                            
+                            Console.WriteLine("Incorrect.\nWrite a number from the menu.\n");
+                            continue;
+                        }
+                }
+            }                    
+        }
+        static void raceStatus(List<Car> allcars)
+        {
+            foreach (Car item in allcars)
+            {
+                if (item.Distance >= item.raceLength)
+                {
+                    Console.WriteLine("{0} have finished the race!", item.Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} have raced {1:0.###} km. Current speed is: {2} km/h", item.Name, item.Distance / 1000, item.Speed * item.carStop);
+                }
+            }
+        }
+    
+    }
+}
